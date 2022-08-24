766be00190f746d1ef99b9ab1959afe414eb5d2a
Ian Neo <ian.neo@ecloudvalley.com>
2022-08-25 01:06:48 +0800

    Update eslint rules

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,17 +1,25 @@
 import { configure as serverlessExpress } from '@vendia/serverless-express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+    APIGatewayProxyEvent,
+    APIGatewayProxyResult,
+    Context,
+} from 'aws-lambda';
 
 let cachedServer;
 
-export const handler = async (event, context) => {
-  if (!cachedServer) {
-    const nestApp = await NestFactory.create(AppModule);
-    await nestApp.init();
-    cachedServer = serverlessExpress({
-      app: nestApp.getHttpAdapter().getInstance(),
-    });
-  }
+export const handler = async (
+    event: APIGatewayProxyEvent,
+    context: Context,
+): Promise<APIGatewayProxyResult> => {
+    if (!cachedServer) {
+        const nestApp = await NestFactory.create(AppModule);
+        await nestApp.init();
+        cachedServer = serverlessExpress({
+            app: nestApp.getHttpAdapter().getInstance(),
+        });
+    }
 
-  return cachedServer(event, context);
+    return cachedServer(event, context);
 };
