49dab21dc368fe1a68a49d3b1f82999cf79020eb
Ian Neo <ian.neo@ecloudvalley.com>
2022-08-27 01:34:15 +0800

    Setup common resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { ZodValidationPipe } from '@anatine/zod-nestjs';
 import { Module } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
-import { FileModule } from './api/file/file.module';
 
 @Module({
-    imports: [FileModule],
+    imports: [],
     controllers: [],
     providers: [{ provide: APP_PIPE, useClass: ZodValidationPipe }],
 })
