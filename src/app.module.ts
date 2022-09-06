cc3b356726e0000cf7be92aa5b42be70bb788bc8
Ian Neo <ian.neo@ecloudvalley.com>
2022-09-06 23:19:59 +0800

    Added back user controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { ZodValidationPipe } from '@anatine/zod-nestjs';
 import { Module } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
+import { UserModule } from './user/user.module';
 
 @Module({
-    imports: [],
+    imports: [UserModule],
     controllers: [],
     providers: [{ provide: APP_PIPE, useClass: ZodValidationPipe }],
 })
